2b197541ffdcd6a9ab99635ab2a1d0ff6e2afadf
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-05-08 18:18:37 -0300

    Initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello React Native</Text>
+    </View>
+  );
+};
+
+export default App;
