e6807eaf7c3aa2a7ed84332dc1f45bd98a8e500a
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-06-15 01:40:43 -0300

    update pages signIn, signUp, hooks/auth, app.routes, auth.routes

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,18 +1,19 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 190 : 40}px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
-  margin: 64px 0 24px;
+  margin: 40px 0 10px 0;
 `;
 
 export const ForgotPassword = styled.TouchableOpacity`
